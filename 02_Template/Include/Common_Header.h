70f7bd711b89043dba7d9f087630c0a668d6b33b
MALATI <pournimap28@gmail.com>
2020-02-23 12:15:11 +0530

    Added new Template included ModelLoading

---
+++ 02_Template/Include/Common_Header.h
@@ -0,0 +1,169 @@
+#include <windows.h>
+#include <windowsx.h>
+
+#include <stdio.h> // for FILE I/O
+#include <stdint.h> //for uint64_t
+#include <string>
+
+#include <C:\glew\include\GL\glew.h>//use it before other GL.h
+
+#include <gl/GL.h>
+
+#include "math_lib_headers/vmath.h"
+#include "Camera/Camera_2.h"
+
+// vmath namespace inclusion
+using namespace vmath;
+
+//#pragma comment(lib,"User32.lib")
+//#pragma comment(lib,"GDI32.lib")
+#pragma comment (lib, "opengl32.lib")
+#pragma comment (lib, "C:\\glew\\lib\\Release\\x64\\glew32.lib")
+
+
+#define WIN_WIDTH 800
+#define WIN_HEIGHT 600
+
+enum
+{
+	MATRIX_ATTRIBUTE_POSITION = 0,
+	MATRIX_ATTRIBUTE_COLOR,
+	MATRIX_ATTRIBUTE_NORMAL,
+	MATRIX_ATTRIBUTE_TEXTURE0,
+};
+
+// Global Variable Declarations
+FILE* gpFile = NULL;
+bool gbLight = false;
+mat4 gPerspectiveProjectionMatrix;
+
+
+
+HDC		ghdc = NULL;
+HGLRC	ghrc = NULL;
+HWND	ghwnd = NULL;
+
+bool bDone = false;
+bool gbFullScreen = false;
+bool gbActiveWindow = false;
+
+
+DWORD dwStyle;
+WINDOWPLACEMENT wpPrev = { sizeof(WINDOWPLACEMENT) };
+
+//camera
+Camera Scene_camera;
+bool FirstMouse = true;
+GLfloat lastX = (float)WIN_WIDTH / 2.0;
+GLfloat lastY = (float)WIN_HEIGHT / 2.0;
+GLfloat midX = WIN_WIDTH / 2.0f;
+GLfloat midY = WIN_HEIGHT / 2.0f;
+GLfloat deltaTime = 0.0f;
+GLfloat currentFrame = 0.0f, lastFrame = 0.0f;
+glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+float _yaw = -90.0f;
+float _pitch = 0.0f;
+
+void programObjectSafeRelease(GLuint shaderProgramObject) {
+	// code
+	if (shaderProgramObject) {
+		GLsizei shaderCount;
+		GLsizei shaderNumber;
+
+		glUseProgram(shaderProgramObject);
+
+		glGetProgramiv(shaderProgramObject, GL_ATTACHED_SHADERS, &shaderCount);
+
+		GLuint* pShaders = (GLuint*)malloc(sizeof(GLuint) * shaderCount);
+
+		if (pShaders) {
+			glGetAttachedShaders(shaderProgramObject, shaderCount, &shaderCount, pShaders);
+
+			for (shaderNumber = 0; shaderNumber < shaderCount; shaderNumber++) {
+				glDetachShader(shaderProgramObject, pShaders[shaderNumber]);
+
+				glDeleteShader(pShaders[shaderNumber]);
+				pShaders[shaderNumber] = 0;
+			}
+
+			free(pShaders);
+		}
+
+		glDeleteProgram(shaderProgramObject);
+		shaderProgramObject = 0;
+
+		glUseProgram(0);
+	}
+}
+
+#pragma region Log
+void checkCompilationLog(char* shaderObjectName, GLuint object) {
+	// function declaration
+	void uninitialize(int);
+
+	// variable declaration
+	GLint iShaderCompileStatus = 0;
+	GLint iInfoLogLength = 0;
+	GLchar* szInfoLog = NULL;
+
+	// code
+	glGetShaderiv(object, GL_COMPILE_STATUS, &iShaderCompileStatus);
+
+	if (iShaderCompileStatus == GL_FALSE) {
+		glGetShaderiv(object, GL_INFO_LOG_LENGTH, &iInfoLogLength);
+
+		if (iInfoLogLength > 0) {
+			szInfoLog = (GLchar*)malloc(sizeof(GLchar) * iInfoLogLength);
+
+			if (szInfoLog != NULL) {
+				GLsizei written;
+
+				glGetShaderInfoLog(object, iInfoLogLength, &written, szInfoLog);
+
+				fprintf(gpFile, "\n\n%s Compilation Error: \n%s\n\n", shaderObjectName, szInfoLog);
+
+				glDeleteShader(object);
+				free(szInfoLog);
+				uninitialize(EXIT_FAILURE);
+				DestroyWindow(ghwnd);
+				exit(0);
+			}
+		}
+	}
+}
+
+void checkLinkLog(char* programObjectName, GLuint programObject) {
+	// function declaration
+	void uninitialize(int);
+
+	// variable declarations
+	GLint iProgramLinkStatus = 0;
+	GLint iInfoLogLength = 0;
+	GLchar* szInfoLog = NULL;
+
+	// code
+	glGetProgramiv(programObject, GL_LINK_STATUS, &iProgramLinkStatus);
+
+	if (iProgramLinkStatus == GL_FALSE) {
+		glGetProgramiv(programObject, GL_INFO_LOG_LENGTH, &iInfoLogLength);
+
+		if (iInfoLogLength > 0) {
+			szInfoLog = (GLchar*)malloc(sizeof(GLchar) * iInfoLogLength);
+
+			if (szInfoLog != NULL) {
+				GLsizei written;
+
+				glGetProgramInfoLog(programObject, iInfoLogLength, &written, szInfoLog);
+
+				fprintf(gpFile, "\n\n%s Linking Error: \n%s\n\n", programObjectName, szInfoLog);
+
+				free(szInfoLog);
+				uninitialize(EXIT_FAILURE);
+				DestroyWindow(ghwnd);
+				exit(0);
+			}
+		}
+	}
+}
+#pragma endregion
---
+++ 02_Template/Scene/PerFragment.h
@@ -0,0 +1,329 @@
+#pragma once
+
+#include "../Include/BasicShapeLoader/Model/Matrix_Obj_Loader.h"
+
+GLuint gVertexShaderObject_perFragmentLight;
+GLuint gFragmentShaderObject_perFragmentLight;
+GLuint gShaderProgramObject_perFragmentLight;
+
+
+GLuint gModelMatrixUniform_perFragmentLight;
+GLuint gViewMatrixUniform_perFragmentLight;
+GLuint gProjectionMatrixUniform_perFragmentLight;
+GLuint gLdUniform_perFragmentLight, gLaUniform_perFragmentLight, gLsUniform_perFragmentLight;
+GLuint gLightPositionUniform_perFragmentLight;
+GLuint gKdUniform_perFragmentLight, gKaUniform_perFragmentLight, gKsUniform_perFragmentLight;
+GLuint gMaterialShininessUniform_perFragmentLight;
+GLuint gLKeyPressedUniform_perFragmentLight;
+GLuint gTextureSamplerUniform_perFragmentLight, gTextureActiveUniform_perFragmentLight, gAlphaUniform_perFragmentLight;
+
+
+GLfloat lightAmbient[] = { 0.0f,0.0f,0.0f,1.0f };
+GLfloat lightDiffuse[] = { 1.0f,1.0f,1.0f,1.0f };
+GLfloat lightSpecular[] = { 1.0f,1.0f,1.0f,1.0f };
+GLfloat lightPosition[] = { 100.0f,100.0f,100.0f,1.0f };
+
+GLfloat material_ambient[] = { 0.0f,0.0f,0.0f,1.0f };
+GLfloat material_diffuse[] = { 1.0f,1.0f,1.0f,1.0f };
+GLfloat material_specular[] = { 1.0f,1.0f,1.0f,1.0f };
+GLfloat material_shininess = 50.0f;
+
+void initPerFragmentShader()
+{
+	void uninitialize(int);
+
+	//VERTEX SHADER
+	gVertexShaderObject_perFragmentLight = glCreateShader(GL_VERTEX_SHADER);
+
+	const GLchar* vertextShaderSourceCode =
+		"#version 400" \
+		"\n" \
+		"in vec4 vPosition;" \
+		"in vec3 vNormal;" \
+		"in vec2 vTexcoord;" \
+
+		"uniform mat4 u_model_matrix;" \
+		"uniform mat4 u_view_matrix;" \
+		"uniform mat4 u_projection_matrix;" \
+		"uniform vec4 u_light_position;"\
+		"uniform int u_LKeyPressed;" \
+
+		"out vec3 transformed_normals;" \
+		"out vec3 light_direction;" \
+		"out vec3 viewer_vector;" \
+		"out  vec2 out_texcord;" \
+
+		"void main(void)" \
+		"{" \
+		"if(u_LKeyPressed == 1)" \
+		"{" \
+		"vec4 eye_coordinates=u_view_matrix * u_model_matrix * vPosition;" \
+		"transformed_normals=mat3(u_view_matrix * u_model_matrix) * vNormal;" \
+		"light_direction = vec3(u_light_position) - eye_coordinates.xyz;" \
+		"viewer_vector = -eye_coordinates.xyz;" \
+		"}"\
+
+		"out_texcord = vTexcoord;" \
+		"gl_Position = u_projection_matrix * u_view_matrix * u_model_matrix * vPosition;" \
+		"}";
+
+	glShaderSource(gVertexShaderObject_perFragmentLight, 1, (const GLchar**)&vertextShaderSourceCode, NULL);
+
+	//compile shader
+	glCompileShader(gVertexShaderObject_perFragmentLight);
+
+	// Error checking
+	checkCompilationLog((char*)"gVertexShaderObject_perFragmentLight", gVertexShaderObject_perFragmentLight);
+
+
+	//FRAGMENT SHADER
+
+	gFragmentShaderObject_perFragmentLight = glCreateShader(GL_FRAGMENT_SHADER);
+
+	const GLchar* fragmentShaderSourceCode =
+		"#version 400" \
+		"\n" \
+		"in vec3 transformed_normals;" \
+		"in vec3 light_direction;" \
+		"in vec3 viewer_vector;" \
+		"in vec2 out_texcord;" \
+
+		"out vec4 FragColor;" \
+
+		"uniform vec3 u_La;" \
+		"uniform vec3 u_Ld;" \
+		"uniform vec3 u_Ls;" \
+		"uniform vec3 u_Ka;" \
+		"uniform vec3 u_Kd;" \
+		"uniform vec3 u_Ks;" \
+		"uniform float u_material_shininess;" \
+		"uniform int u_LKeyPressed;" \
+		"uniform sampler2D u_texture0_sampler;"\
+		"uniform int u_is_texture;" \
+		"uniform float u_alpha;" \
+
+		"void main(void)" \
+		"{" \
+		"vec3 phong_ads_color;" \
+		"vec4 Final_Texture;" \
+		"if(u_LKeyPressed==1)" \
+		"{" \
+		"vec3 normalized_transformed_normals=normalize(transformed_normals);" \
+		"vec3 normalized_light_direction=normalize(light_direction);" \
+		"vec3 normalized_viewer_vector=normalize(viewer_vector);" \
+		"vec3 ambient = u_La * u_Ka;" \
+		"float tn_dot_ld = max(dot(normalized_transformed_normals, normalized_light_direction),0.0);" \
+		"vec3 diffuse = u_Ld * u_Kd * tn_dot_ld;" \
+		"vec3 reflection_vector = reflect(-normalized_light_direction, normalized_transformed_normals);" \
+		"vec3 specular = u_Ls * u_Ks * pow(max(dot(reflection_vector, normalized_viewer_vector), 0.0), u_material_shininess);" \
+		"phong_ads_color=ambient + diffuse + specular;" \
+		"}" \
+		"else" \
+		"{" \
+		"phong_ads_color = vec3(1.0, 1.0, 1.0);" \
+		"}" \
+		"if(u_is_texture == 1)" \
+		"{" \
+		"Final_Texture = texture(u_texture0_sampler, out_texcord); " \
+		"FragColor = vec4(phong_ads_color, u_alpha) * Final_Texture;" \
+		"}" \
+		"else" \
+		"{" \
+		"FragColor = vec4(phong_ads_color, u_alpha);" \
+		"}" \
+		"}";
+
+	glShaderSource(gFragmentShaderObject_perFragmentLight, 1, (const GLchar**)&fragmentShaderSourceCode, NULL);
+
+	glCompileShader(gFragmentShaderObject_perFragmentLight);
+
+	// Error checking
+	checkCompilationLog((char*)"gFragmentShaderObject_perFragmentLight", gFragmentShaderObject_perFragmentLight);
+
+
+	//Shader Program
+
+	gShaderProgramObject_perFragmentLight = glCreateProgram();
+
+	glAttachShader(gShaderProgramObject_perFragmentLight, gVertexShaderObject_perFragmentLight);
+
+	glAttachShader(gShaderProgramObject_perFragmentLight, gFragmentShaderObject_perFragmentLight);
+
+	glBindAttribLocation(gShaderProgramObject_perFragmentLight, MATRIX_ATTRIBUTE_POSITION, "vPosition");
+	glBindAttribLocation(gShaderProgramObject_perFragmentLight, MATRIX_ATTRIBUTE_NORMAL, "vNormal");
+	glBindAttribLocation(gShaderProgramObject_perFragmentLight, MATRIX_ATTRIBUTE_TEXTURE0, "vTexcoord");
+
+	glLinkProgram(gShaderProgramObject_perFragmentLight);
+
+	// Error checking
+	checkLinkLog((char*)"gShaderProgramObject_perFragmentLight", gShaderProgramObject_perFragmentLight);
+
+
+	gModelMatrixUniform_perFragmentLight = glGetUniformLocation(gShaderProgramObject_perFragmentLight, "u_model_matrix");
+	gViewMatrixUniform_perFragmentLight = glGetUniformLocation(gShaderProgramObject_perFragmentLight, "u_view_matrix");
+	gProjectionMatrixUniform_perFragmentLight = glGetUniformLocation(gShaderProgramObject_perFragmentLight, "u_projection_matrix");
+
+	gLKeyPressedUniform_perFragmentLight = glGetUniformLocation(gShaderProgramObject_perFragmentLight, "u_LKeyPressed");
+
+	gLdUniform_perFragmentLight = glGetUniformLocation(gShaderProgramObject_perFragmentLight, "u_Ld");
+	gLaUniform_perFragmentLight = glGetUniformLocation(gShaderProgramObject_perFragmentLight, "u_La");
+	gLsUniform_perFragmentLight = glGetUniformLocation(gShaderProgramObject_perFragmentLight, "u_Ls");
+
+	gKdUniform_perFragmentLight = glGetUniformLocation(gShaderProgramObject_perFragmentLight, "u_Kd");
+	gKsUniform_perFragmentLight = glGetUniformLocation(gShaderProgramObject_perFragmentLight, "u_Ks");
+	gKaUniform_perFragmentLight = glGetUniformLocation(gShaderProgramObject_perFragmentLight, "u_Ka");
+
+	gLightPositionUniform_perFragmentLight = glGetUniformLocation(gShaderProgramObject_perFragmentLight, "u_light_position");
+	gMaterialShininessUniform_perFragmentLight = glGetUniformLocation(gShaderProgramObject_perFragmentLight, "u_material_shininess");
+
+	gTextureSamplerUniform_perFragmentLight = glGetUniformLocation(gShaderProgramObject_perFragmentLight, "u_texture0_sampler");
+
+	gTextureActiveUniform_perFragmentLight = glGetUniformLocation(gShaderProgramObject_perFragmentLight, "u_is_texture");
+	gAlphaUniform_perFragmentLight = glGetUniformLocation(gShaderProgramObject_perFragmentLight, "u_alpha");
+}
+
+
+void initialize_perFragmentLight()
+{
+	initPerFragmentShader();
+
+	initSphereShape();
+
+	LoadAllModels();
+
+	glShadeModel(GL_SMOOTH);
+
+	glClearDepth(1.0f);
+	// enable depth testing
+	glEnable(GL_DEPTH_TEST);
+	// depth test to do
+	glDepthFunc(GL_LEQUAL);
+
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+	glEnable(GL_CULL_FACE);
+
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f); // blue
+
+}
+
+void display_perFragmentLight()
+{
+	glUseProgram(gShaderProgramObject_perFragmentLight);
+
+	if (gbLight == true)
+	{
+		glUniform1i(gLKeyPressedUniform_perFragmentLight, 1);
+
+		glUniform3fv(gLdUniform_perFragmentLight, 1, lightDiffuse);//white
+		glUniform3fv(gLaUniform_perFragmentLight, 1, lightAmbient);
+		glUniform3fv(gLsUniform_perFragmentLight, 1, lightSpecular);
+		glUniform4fv(gLightPositionUniform_perFragmentLight, 1, lightPosition);
+
+		/*glUniform3fv(gKdUniform_perFragmentLight, 1, material_diffuse);
+		glUniform3fv(gKaUniform_perFragmentLight, 1, material_ambient);
+		glUniform3fv(gKsUniform_perFragmentLight, 1, material_specular);
+		glUniform1f(gMaterialShininessUniform_perFragmentLight, material_shininess);*/
+
+	}
+	else
+	{
+		glUniform1i(gLKeyPressedUniform_perFragmentLight, 0);
+	}
+
+	mat4 modelMatrix = mat4::identity();
+	glm::mat4 viewMatrix = glm::mat4();
+	mat4 scaleMatrix = mat4::identity();
+
+	viewMatrix = Scene_camera.GetViewMatrix();
+	glUniformMatrix4fv(gViewMatrixUniform_perFragmentLight, 1, GL_FALSE, &viewMatrix[0][0]);
+
+	modelMatrix = vmath::translate(0.0f, 0.0f, -2.0f);
+
+	glUniformMatrix4fv(gProjectionMatrixUniform_perFragmentLight, 1, GL_FALSE, gPerspectiveProjectionMatrix);
+
+
+	
+	//object
+	//Draw Mahal Model
+	modelMatrix = mat4::identity();
+	modelMatrix = vmath::translate(0.0f, -2.0f, -6.0f);
+	scaleMatrix = scale(10.0f, 10.0f, 10.0f);
+	modelMatrix = modelMatrix * scaleMatrix;
+	glUniformMatrix4fv(gModelMatrixUniform_perFragmentLight, 1, GL_FALSE, modelMatrix);
+	glBindVertexArray(gModel_Mahal.Vao);
+
+	for (int i = 0; i < gModel_Mahal.model_mesh_data.size(); i++)
+	{
+		if (gbLight == true)
+		{
+			glUniform3fv(gKaUniform_perFragmentLight, 1, gModel_Mahal.model_material[gModel_Mahal.model_mesh_data[i].material_index].Ka);
+			glUniform3fv(gKdUniform_perFragmentLight, 1, gModel_Mahal.model_material[gModel_Mahal.model_mesh_data[i].material_index].Kd);
+			glUniform3fv(gKsUniform_perFragmentLight, 1, gModel_Mahal.model_material[gModel_Mahal.model_mesh_data[i].material_index].Ks);
+			glUniform1f(gMaterialShininessUniform_perFragmentLight, material_shininess);
+			glUniform1f(gAlphaUniform_perFragmentLight, gModel_Mahal.model_material[gModel_Mahal.model_mesh_data[i].material_index].d);
+
+			if (gModel_Mahal.model_material[gModel_Mahal.model_mesh_data[i].material_index].ismap_Kd == true)
+			{
+				glActiveTexture(GL_TEXTURE0);
+				glBindTexture(GL_TEXTURE_2D, gModel_Mahal.model_material[gModel_Mahal.model_mesh_data[i].material_index].gTexture);
+				glUniform1i(gTextureSamplerUniform_perFragmentLight, 0);
+				glUniform1i(gTextureActiveUniform_perFragmentLight, 1);
+				/*fprintf(gpFile, "perFragmentLight Mahal ismapKd true \n");
+				fflush(gpFile);*/
+			}
+			else
+				glUniform1i(gTextureActiveUniform_perFragmentLight, 0);
+		}
+		glDrawArrays(GL_TRIANGLES, gModel_Mahal.model_mesh_data[i].vertex_Index, gModel_Mahal.model_mesh_data[i].vertex_Count);
+	}
+	glBindVertexArray(0);
+	modelMatrix = mat4::identity();
+	modelMatrix = vmath::translate(0.0f, 0.0f, -6.0f);
+	scaleMatrix = scale(10.0f, 10.0f, 10.0f);
+	modelMatrix = modelMatrix * scaleMatrix;
+	glUniformMatrix4fv(gModelMatrixUniform_perFragmentLight, 1, GL_FALSE, modelMatrix);
+	//Draw Krishna Model
+	glBindVertexArray(gModel_Krishna.Vao);
+	for (int i = 0; i < gModel_Krishna.model_mesh_data.size(); i++)
+	{
+		if (gbLight == true)
+		{
+			glUniform3fv(gKaUniform_perFragmentLight, 1, gModel_Krishna.model_material[gModel_Krishna.model_mesh_data[i].material_index].Ka);
+			glUniform3fv(gKdUniform_perFragmentLight, 1, gModel_Krishna.model_material[gModel_Krishna.model_mesh_data[i].material_index].Kd);
+			glUniform3fv(gKsUniform_perFragmentLight, 1, gModel_Krishna.model_material[gModel_Krishna.model_mesh_data[i].material_index].Ks);
+			glUniform1f(gMaterialShininessUniform_perFragmentLight, material_shininess);
+			glUniform1f(gAlphaUniform_perFragmentLight, gModel_Krishna.model_material[gModel_Krishna.model_mesh_data[i].material_index].d);
+			if (gModel_Krishna.model_material[gModel_Krishna.model_mesh_data[i].material_index].ismap_Kd == true)
+			{
+				glActiveTexture(GL_TEXTURE0);
+				glBindTexture(GL_TEXTURE_2D, gModel_Krishna.model_material[gModel_Krishna.model_mesh_data[i].material_index].gTexture);
+				glUniform1i(gTextureSamplerUniform_perFragmentLight, 0);
+				glUniform1i(gTextureActiveUniform_perFragmentLight, 1);
+				/*fprintf(gpFile, "perFragmentLight Krishna ismapKd true \n");
+				fflush(gpFile);*/
+			}
+			else
+				glUniform1i(gTextureActiveUniform_perFragmentLight, 0);
+		}
+		glDrawArrays(GL_TRIANGLES, gModel_Krishna.model_mesh_data[i].vertex_Index, gModel_Krishna.model_mesh_data[i].vertex_Count);
+	}
+	glBindVertexArray(0);
+
+
+	glUseProgram(0);
+}
+
+void update_perFragmentLight()
+{
+
+}
+
+void uninitialize_perFragmentLight()
+{
+	uninitializeAllModelData();
+
+	uninitializeSphereShape();
+
+	programObjectSafeRelease(gShaderProgramObject_perFragmentLight);
+
+}
