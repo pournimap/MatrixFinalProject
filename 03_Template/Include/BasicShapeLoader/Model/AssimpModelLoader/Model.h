ef12d4e576f5ecab4008230cdd383cadc28bd73d
MALATI <pournimap28@gmail.com>
2020-04-20 09:41:00 +0530

    normals changed

--- 03_Template/Include/BasicShapeLoader/Model/AssimpModelLoader/Model.h
+++ 03_Template/Include/BasicShapeLoader/Model/AssimpModelLoader/Model.h
@@ -311,7 +311,7 @@ Mesh Model::processMesh(aiMesh* mesh, const aiScene* scene)
 		std::vector<Texture> specularMaps = loadMaterialTextures(material, aiTextureType_SPECULAR, "texture_specular");
 		textures.insert(textures.end(), specularMaps.begin(), specularMaps.end());
 		// 3. normal maps
-		std::vector<Texture> normalMaps = loadMaterialTextures(material, aiTextureType_HEIGHT, "texture_normal");
+		std::vector<Texture> normalMaps = loadMaterialTextures(material, aiTextureType_NORMALS, "texture_normal");
 		textures.insert(textures.end(), normalMaps.begin(), normalMaps.end());
 		// 4. height maps
 		std::vector<Texture> heightMaps = loadMaterialTextures(material, aiTextureType_AMBIENT, "texture_height");
@@ -378,6 +378,9 @@ std::vector<Texture> Model::loadMaterialTextures(aiMaterial* mat, aiTextureType 
 
 		std::string filename = std::string(ai_str.C_Str());
 		filename = directory + '/' + filename;
+
+		fprintf(gpFile, "Texture::%s of type_name:%s\n", ai_str.C_Str(), type_name.c_str());
+		fflush(gpFile);
 
 		Texture texture;
 		//texture.id = TextureFromFile(filename.c_str()); // return prepaired openGL texture
